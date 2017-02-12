6e3e923d54a9e78dddacbd1634bb0719541621b6
Oussema Trigui <oussematrigui@MacBook-Pro-de-Oussema.local>
2017-02-12 19:01:19 +0100

    premier commit

---
+++ src/main/java/com/iss/Hello.java
@@ -0,0 +1,12 @@
+package com.iss;
+
+/**
+ * Created by oussematrigui on 12.02.17.
+ */
+public class Hello {
+     public String sayHello(String name){
+         System.out.println(name);
+         return name ;
+     }
+
+}
